c591dd310f0c9b6c41ed0d6028fe6e5df0e510a1
floLfg <florian.laforgue@neoxia.com>
2020-03-24 01:05:59 +0100

    A resource can provide resource units

--- src/entities/resource.ts
+++ src/entities/resource.ts
@@ -15,4 +15,18 @@ export default class Resource extends Entity {
   public get stock(): number {
     return this._stock;
   }
+
+  public provideResourceUnits(amount: number): number {
+    let providedResourceUnits = 0;
+
+    if (amount > this._stock) {
+      providedResourceUnits = this._stock;
+      this._stock = 0;
+    } else {
+      this._stock -= amount;
+      providedResourceUnits = amount;
+    }
+
+    return providedResourceUnits;
+  }
 }
