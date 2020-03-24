38e73af2ea925ea881a2fa43328457f62b1d9ce3
floLfg <florian.laforgue@neoxia.com>
2020-03-24 01:05:25 +0100

    Rename typing file

--- src/types/qodio-api.ts
+++
@@ -1,3 +0,0 @@
-export type DroneAction = 'waiting' | 'scouting';
-
-export type Axis = 'x' | 'y';
---
+++ src/types/qodio-server.ts
@@ -0,0 +1,3 @@
+export type DroneAction = 'waiting' | 'scouting' | 'gathering';
+
+export type Axis = 'x' | 'y';
