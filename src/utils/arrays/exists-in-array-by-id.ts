03d55e42494ffc4c09a19e27d72cf73dca3e98de
flaforgue <florian.laforgue@neoxia.com>
2020-05-03 23:18:09 +0200

    Improve type of existsInArrayById util

--- src/utils/arrays/exists-in-array-by-id.ts
+++ src/utils/arrays/exists-in-array-by-id.ts
@@ -1,3 +1,5 @@
-export default (arr: { id: string }[], id: string): boolean => {
+import { IdEntity } from '../../entities';
+
+export default <T extends IdEntity>(arr: T[], id: string): boolean => {
   return arr.some((elem) => elem.id === id);
 };
